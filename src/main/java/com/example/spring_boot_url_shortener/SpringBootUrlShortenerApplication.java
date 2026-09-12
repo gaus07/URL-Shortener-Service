6dfa64d3182d12274cbcb8280db0327a6f402b98
gaus07 <gausnoori07@gmail.com>
2026-09-12 13:21:52 +0530

    Eestablished DB connection, define entities and made a home page structure

---
+++ src/main/java/com/example/spring_boot_url_shortener/SpringBootUrlShortenerApplication.java
@@ -0,0 +1,11 @@
+package com.example.spring_boot_url_shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUrlShortenerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUrlShortenerApplication.class, args);
+	}
+}
